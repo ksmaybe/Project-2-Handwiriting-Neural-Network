7b7ef05212979a73ce4e20c3b6c74f284b9d604b
Mr Grey <ksmaybe@gmail.com>
2018-12-07 16:46:12 -0500

    stuck on c compiler

--- neural network.py
+++ neural network.py
@@ -1,7 +1,17 @@
 import numpy as np
 import cv2
 import os
+import math
+from subprocess import call
 
+def sigmoid(x):
+    res=1/(1+math.e^(-x))
+    return res
+
+def sigmoid_derivative(x):
+    f=sigmoid(x)
+    res=f*(1-f)
+    return res
 
 class NeuralNetwork:
     def __init__(self, x, y):
@@ -11,9 +21,20 @@ class NeuralNetwork:
         self.y          = y
         self.output     = np.zeros(y.shape)
 
+    def feedforward(self):
+        self.layer1 = sigmoid(np.dot(self.input, self.weights1))
+        self.output = sigmoid(np.dot(self.layer1, self.weights2))
 
+    def backprop(self):
+        # application of the chain rule to find derivative of the loss function with respect to weights2 and weights1
+        d_weights2 = np.dot(self.layer1.T, (2*(self.y - self.output) * sigmoid_derivative(self.output)))
+        d_weights1 = np.dot(self.input.T,  (np.dot(2*(self.y - self.output) * sigmoid_derivative(self.output), self.weights2.T) * sigmoid_derivative(self.layer1)))
 
-image=cv2.imread("train_img/train_image_0.bmp",1)
+        # update the weights with the derivative (slope) of the loss function
+        self.weights1 += d_weights1
+        self.weights2 += d_weights2
+
+image=cv2.imread("train_img/train_image_2.bmp",1)
 cv2.imshow('image',image)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
