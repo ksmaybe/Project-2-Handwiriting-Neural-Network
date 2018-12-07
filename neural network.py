1ade49236f0e37e2049e58bb4c602a8b0198495e
Mr Grey <ksmaybe@gmail.com>
2018-12-07 18:14:10 -0500

    cleared c

--- neural network.py
+++ neural network.py
@@ -2,7 +2,7 @@ import numpy as np
 import cv2
 import os
 import math
-from subprocess import call
+
 
 def sigmoid(x):
     res=1/(1+math.e^(-x))
@@ -13,28 +13,15 @@ def sigmoid_derivative(x):
     res=f*(1-f)
     return res
 
-class NeuralNetwork:
-    def __init__(self, x, y):
-        self.input      = x
-        self.weights1   = np.random.rand(self.input.shape[1],4)
-        self.weights2   = np.random.rand(4,1)
-        self.y          = y
-        self.output     = np.zeros(y.shape)
+class Connection:
+    def __init__(self, con_neuron):
+        self.con_neuron=con_neuron
+        self.weight=np.random.normal()
+        self.dWeight = 0.0
 
-    def feedforward(self):
-        self.layer1 = sigmoid(np.dot(self.input, self.weights1))
-        self.output = sigmoid(np.dot(self.layer1, self.weights2))
 
-    def backprop(self):
-        # application of the chain rule to find derivative of the loss function with respect to weights2 and weights1
-        d_weights2 = np.dot(self.layer1.T, (2*(self.y - self.output) * sigmoid_derivative(self.output)))
-        d_weights1 = np.dot(self.input.T,  (np.dot(2*(self.y - self.output) * sigmoid_derivative(self.output), self.weights2.T) * sigmoid_derivative(self.layer1)))
 
-        # update the weights with the derivative (slope) of the loss function
-        self.weights1 += d_weights1
-        self.weights2 += d_weights2
 
-image=cv2.imread("train_img/train_image_2.bmp",1)
-cv2.imshow('image',image)
-cv2.waitKey(0)
-cv2.destroyAllWindows()
+image=cv2.imread("train_img/train_image_2.bmp",0)
+img=cv2.bitwise_not(image)
+print(img)
