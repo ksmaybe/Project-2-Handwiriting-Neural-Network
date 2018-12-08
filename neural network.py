9e8b38cf7f66e51c860235914b4ed88374e3afd5
Mr Grey <ksmaybe@gmail.com>
2018-12-08 13:31:58 -0500

    basic network working

--- neural network.py
+++ neural network.py
@@ -4,12 +4,11 @@ import math
 
 
 def sigmoid(x):
-    res=1/(1+math.e^(-x))
+    res=1/(1+math.e**(-x))
     return res
 
 def sigmoid_derivative(x):
-    f=sigmoid(x)
-    res=f*(1-f)
+    res=x*(1-x)
     return res
 
 class Connection:
@@ -42,7 +41,7 @@ class Neuron:
         self.error=error
 
     def set_output(self,output):
-        self.ouput=output
+        self.output=output
 
     def get_output(self):
         return self.output
@@ -51,7 +50,7 @@ class Neuron:
         sum=0
         if len(self.connectors)!=0:
             for con in self.connectors:
-                sum+=con.con_neuron.output*con.weight
+                sum+=con.con_neuron.get_output()*con.weight
         else:
             return
         self.output=sigmoid(sum)
@@ -59,7 +58,7 @@ class Neuron:
     def back_propagate(self):
         self.gradient=self.error*sigmoid_derivative(self.output)
         for con in self.connectors:
-            con.d_weight=Neuron.E*(con.con_neuron.utput*self.gradient)+self.A*con.d_weight
+            con.d_weight=Neuron.E*(con.con_neuron.output*self.gradient)+self.A*con.d_weight
             con.weight+=con.d_weight
             con.con_neuron.add_error(con.weight*self.gradient)
         self.error=0
@@ -91,9 +90,61 @@ class N_Network:
         error/=len(x)
         error=math.sqrt(error)
         return error
-image=cv2.imread("train_img/train_image_2233.bmp",0)
-img=cv2.bitwise_not(image)
-print(img)
-cv2.imshow('image',image)
-cv2.waitKey(0)
-cv2.destroyAllWindows()
+
+    def feed_forward(self):
+        for layer in self.layer_list[1:]:
+            for n in layer:
+                n.feed_forward()
+
+    def back_propagate(self,prev):
+        for i in range(len(prev)):
+            self.layer_list[-1][i].set_error(prev[i]-self.layer_list[-1][i].get_output())
+        for layer in self.layer_list[::-1]:
+            for n in layer:
+                n.back_propagate()
+    def get_results(self):
+        output=[]
+        for n in self.layer_list[-1]:
+            out=n.get_output()
+            if out>0.5:
+                o=1
+            else:
+                o=0
+            output.append(out)
+        output.pop()     #remove bias neuron
+        return output
+
+def main():
+    set=[]
+    set.append(2)
+    set.append(3)
+    set.append(2)
+    net=N_Network(set)
+    Neuron.E=0.09
+    Neuron.A=0.015
+    inputs=[[0,0],[0,1],[1,0],[1,1]]
+    outputs=[[0,0],[1,0],[1,0],[0,1]]
+    while True:
+        err=0
+        for i in range(len(inputs)):
+            net.set_input(inputs[i])
+            net.feed_forward()
+            net.back_propagate(outputs[i])
+            err+=net.get_error(outputs[i])
+        print("error: ",err)
+        if err<0.05:
+            break
+    while True:
+        a=int(input("type 1st input: "))
+        b=int(input("type 2nd input: "))
+        net.set_input([a,b])
+        net.feed_forward()
+        print(net.get_results())
+main()
+
+# image=cv2.imread("train_img/train_image_2233.bmp",0)
+# img=cv2.bitwise_not(image)
+# print(img)
+# cv2.imshow('image',image)
+# cv2.waitKey(0)
+# cv2.destroyAllWindows()
