961ae67f2dd1a9614621b6e51c65163d725afa17
Mr Grey <ksmaybe@gmail.com>
2018-12-08 20:45:14 -0500

    runtime too long

--- neural network.py
+++ neural network.py
@@ -1,6 +1,7 @@
 import numpy as np
 import cv2
 import math
+import os
 
 
 def sigmoid(x):
@@ -58,7 +59,7 @@ class Neuron:
     def back_propagate(self):
         self.gradient=self.error*sigmoid_derivative(self.output)
         for con in self.connectors:
-            con.d_weight=Neuron.E*(con.con_neuron.output*self.gradient)+self.A*con.d_weight
+            con.d_weight=self.E*(con.con_neuron.output*self.gradient)+self.A*con.d_weight
             con.weight+=con.d_weight
             con.con_neuron.add_error(con.weight*self.gradient)
         self.error=0
@@ -82,12 +83,12 @@ class N_Network:
         for i in range(len(input_list)):
             self.layer_list[0][i].set_output(input_list[i])
 
-    def get_error(self,x):
+    def get_error(self,y):
         error=0
-        for i in range(len(x)):
-            err=(x[i]-self.layer_list[-1][i].get_output())
+        for i in range(len(y)):
+            err=(y[i]-self.layer_list[-1][i].get_output())
             error+=err**2
-        error/=len(x)
+        error/=len(y)
         error=math.sqrt(error)
         return error
 
@@ -106,45 +107,102 @@ class N_Network:
         output=[]
         for n in self.layer_list[-1]:
             out=n.get_output()
-            if out>0.5:
-                o=1
-            else:
-                o=0
+            # if out>0.5:
+            #     out=1
+            # else:
+            #     out=0
             output.append(out)
         output.pop()     #remove bias neuron
         return output
 
 def main():
+
+
+
+    #read train image to integer values
+
+    train_lst=[]
+    p="train_img/"
+    x=os.listdir("train_img")
+    no_of_train=10 #len(x)
+    for i in range(no_of_train):
+        image=cv2.imread(p+x[i],0)
+        img=cv2.bitwise_not(image)
+        img1=[]
+        for c in img:
+            img1.extend(c)
+        train_lst.append(img1)
+
+    #read training labels
+    f=open("train_labels.txt",'r')
+    read_lines_train=f.readlines()
+    train_label=[]
+    for line in read_lines_train:
+        mlst=[]
+        for c in line:
+            if c.isnumeric():
+                mlst.append(int(c))
+        train_label.append(mlst)
+    train_label=train_label[:no_of_train]
+
+    #read test image to integer values
+
+    test_lst=[]
+    p="train_img/"
+    k=os.listdir("train_img")
+    no_of_test=300
+    for i in range(no_of_test):
+        image=cv2.imread(p+k[i],0)
+        img=cv2.bitwise_not(image)
+        img1=[]
+        for c in img:
+            img1.extend(c)
+        test_lst.append(img1)
+
+    #read test labels
+    g=open("test_labels.txt",'r')
+    read_lines_test=g.readlines()
+    test_label=[]
+    for line in read_lines_test:
+        mlst=[]
+        for c in line:
+            if c.isnumeric():
+                mlst.append(int(c))
+        test_label.append(mlst)
+    test_label=test_label[:no_of_test]
+
+
+
+    #begin neural network
     set=[]
-    set.append(2)
-    set.append(3)
-    set.append(2)
+    set.append(28*28)
+    set.append(50)
+    set.append(5)
     net=N_Network(set)
     Neuron.E=0.09
     Neuron.A=0.015
-    inputs=[[0,0],[0,1],[1,0],[1,1]]
-    outputs=[[0,0],[1,0],[1,0],[0,1]]
+    inputs=train_lst
+    outputs=train_label
     while True:
         err=0
+        zz=1
         for i in range(len(inputs)):
             net.set_input(inputs[i])
             net.feed_forward()
             net.back_propagate(outputs[i])
-            err+=net.get_error(outputs[i])
-        print("error: ",err)
-        if err<0.05:
+            err=net.get_error(outputs[i])
+            print("error: ",err)
+            print(zz,"output train: ",net.get_results())
+            print("train_label: ", train_label[i])
+            zz+=1
+        print("total err: ",err)
+        if err<0.1:
             break
-    while True:
-        a=int(input("type 1st input: "))
-        b=int(input("type 2nd input: "))
-        net.set_input([a,b])
+    for z in range(no_of_test):
+        k=test_lst[z]
+        net.set_input(k)
         net.feed_forward()
-        print(net.get_results())
+        print("Results: ",net.get_results())
+        print("Label: ",test_label[z])
 main()
 
-# image=cv2.imread("train_img/train_image_2233.bmp",0)
-# img=cv2.bitwise_not(image)
-# print(img)
-# cv2.imshow('image',image)
-# cv2.waitKey(0)
-# cv2.destroyAllWindows()
--- test.py
+++ test.py
@@ -1,2 +1,36 @@
-import math
-print(math.e)
+import cv2
+import os
+import copy
+import rawpy
+import imageio
+import numpy as np
+# #read train image to integer values
+# no_of_train=1
+# train_lst=[]
+# p="train_img/"
+# x=os.listdir("train_img")
+# for i in range(no_of_train):
+#     image=cv2.imread(p+x[i],0)
+#     img=cv2.bitwise_not(image)
+#     img1=[]
+#     for c in img:
+#         img1.extend(c)
+#     print(img1)
+#
+#
+# print(train_lst)
+
+#read train image to integer values
+no_of_train=3000
+train_lst=[]
+p="train_img/"
+x=os.listdir("train_img")
+for i in range(no_of_train):
+    image=cv2.imread(p+x[i],0)
+    img=cv2.bitwise_not(image)
+    img1=[]
+    for c in img:
+        img1.extend(c)
+    train_lst.append(img1)
+
+print(train_lst)
